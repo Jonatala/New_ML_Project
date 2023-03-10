fe5e2cfeedd5fc888b7d059562afeb345b0e8705
JonA <olaatala7@gmail.com>
2023-03-10 17:21:59 +0000

    new commit

--- src/exception.py
+++ src/exception.py
@@ -2,3 +2,10 @@ import sys
 
 def error_message_detail(error, error_detail:sys):
     -, -, exc_tb = error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occurred in python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+
+    )
+
+
